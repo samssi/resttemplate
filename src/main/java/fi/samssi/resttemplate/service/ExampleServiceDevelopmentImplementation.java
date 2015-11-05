7210b11dd3a557d2cb52ca23ad5aac8e269894a9
Sämir Hamadi <samir.hamadi@elisa.fi>
2015-11-05 09:39:45 +0200

    Add properties example to the service

--- src/main/java/fi/samssi/resttemplate/service/ExampleServiceDevelopmentImplementation.java
+++ src/main/java/fi/samssi/resttemplate/service/ExampleServiceDevelopmentImplementation.java
@@ -1,9 +1,15 @@
 package fi.samssi.resttemplate.service;
 
+import fi.samssi.resttemplate.application.property.ApplicationProperties;
 import fi.samssi.resttemplate.domain.ExampleClass;
 
+import javax.inject.Inject;
+
 public class ExampleServiceDevelopmentImplementation implements ExampleService {
+    @Inject
+    private ApplicationProperties applicationProperties;
+
     public ExampleClass fetchExampleClass() {
-        return new ExampleClass("Immutable development context string");
+        return new ExampleClass(applicationProperties.config().getString("example.service.message"));
     }
 }
--- src/main/java/fi/samssi/resttemplate/service/ExampleServiceTestImplementation.java
+++ src/main/java/fi/samssi/resttemplate/service/ExampleServiceTestImplementation.java
@@ -1,10 +1,16 @@
 package fi.samssi.resttemplate.service;
 
+import fi.samssi.resttemplate.application.property.ApplicationProperties;
 import fi.samssi.resttemplate.domain.ExampleClass;
 
+import javax.inject.Inject;
+
 public class ExampleServiceTestImplementation implements ExampleService {
+    @Inject
+    private ApplicationProperties applicationProperties;
+
     @Override
     public ExampleClass fetchExampleClass() {
-        return new ExampleClass("Immutable test context string");
+        return new ExampleClass(applicationProperties.config().getString("example.service.message"));
     }
 }
